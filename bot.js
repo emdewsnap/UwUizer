46063bb5596fe3e2b85f4725595d697d31d519f8
Elizabeth Dewsnap <emdewsnap@gmail.com>
2020-01-25 19:11:01 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setGame("Tag me to UwUize!");
+ });
+
+client.on('message', msg => {
+    if (msg.isMentioned(client.user)) {
+        msg.channel.fetchMessages({ limit: 2 }).then(messages => {
+            let prevMes = messages.last().content;
+            let mesAsArr = prevMes.split("");
+        let newMes = "";
+        for(let x = 0; x < mesAsArr.length; x++){
+            if(mesAsArr[x] == 'l' || mesAsArr[x] == 'r'){
+                mesAsArr[x] = 'w';
+            }
+            if(mesAsArr[x] == 'L' || mesAsArr[x] == 'R'){
+                mesAsArr[x] = 'W';
+            }
+            newMes += mesAsArr[x];
+        }
+        newMes += " OwO";
+        msg.channel.send(newMes);
+
+          })
+          .catch(console.error);
+    }
+});
+
+client.login('NjY5OTk1NTQ4NjQzODE5NTMy.Xin8gQ._1PHC0_QA5rL2QVddjlLG6rFFcs');
